00bd5d16cc9450b575e44627d0a10d372e6b38f4
robertmeans <robert@robertmeans.com>
2015-10-05 21:42:30 -0600

    checking in

--- index-with-levels-of-membership.php
+++ index-with-levels-of-membership.php
@@ -11,8 +11,24 @@
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
 	<link href='https://fonts.googleapis.com/css?family=Courgette|Lato' rel='stylesheet' type='text/css'>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
 	<script src="https://maps.googleapis.com/maps/api/js"></script>
-	<script src="js/google-map.js"></script>
+    <script>
+     function initialize() {
+    var map_canvas = document.getElementById('map');
+    var map_options = {
+        center: new google.maps.LatLng(39.636833, -105.316211),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(map_canvas, map_options);
+    var i=new google.maps.Marker({
+        position:new google.maps.LatLng(39.636833, -105.316211),
+        map:map
+        })
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -29,7 +45,7 @@
 
 <section id="intro">
 	<img src="_images/rookies-logo-02.png">
-	<h2>Evergreen, Colorad<a class="temp-link" href="http://www.rookiesevergreen.com/beta/Rookies-Waiver-and-Rules.pdf" target="_bank">o</a><br>Batting Cages</h2>
+	<h2>Evergreen, Colorado<br>Batting Cages</h2>
 	<div id="join-book">
 		<ul>
 			<li><a class="reservations" href="https://booknow.appointment-plus.com/7qp3lmnc" target="_blank">book now</a></li>
@@ -51,33 +67,45 @@
             <a href="#" class="click no-default">1. Book time in one of our batting cages <i class="fa fa-plus"></i></a>
 
 			<div class="expand">
+              <h2>Levels of Membership:</h2>
 
-				<p class="full-text ex-sp">Book time by the hour or purchase one of our discounted package options and save.</p>
 
-              <h2>Booking Rates:</h2>
+
+<!-- <p class="full-text note"><em>Note</em>: 1<sup>st</sup> time visitors will need to make reservations at least 24 hours in advance in order to provide time to receive a building access code.</p> -->
+
+
+
 				<div class="membership-level-full cf">
-	              <p class="membership-level-left">10 for $350</p>
-	              <p class="membership-level-right">Buy 10 1 hour sessions and save $50.</p>
-	              <p class="membership-level-right">When you buy a block of 10 sessions your account receives 10 credits that you can use towards future cage rentals at your convenience.</p>
+
+
+
+
+
+	              <p class="membership-level-left">The Show</p>
+	              <p class="membership-level-right">$150/month includes 4/1hr Peak Time &amp; 4/1hr Non-Peak Time rentals of 1 tunnel (6 month minimum) *$18.75/hr</p>
 				</div>
 				<div class="membership-level-full cf">
-	              <p class="membership-level-left">4 for $150</p>
-	              <p class="membership-level-right">Buy 4 1 hour sessions and save $10.</p>
-	              <p class="membership-level-right">When you buy a block of 4 sessions your account receives 4 credits that you can use towards future cage rentals at your convenience.</p>
+	              <p class="membership-level-left">Triple A</p>
+	              <p class="membership-level-right">Purchase 9 innings and get the 10<sup>th</sup> free. This is the punch card option which allows you to purchase a block of 9/1hr rentals of 1 tunnel and receive the 10<sup>th</sup> free. The cost for Triple A is $360.00 and there are no restrictions between Peak &amp; Non-Peak hours. *$36/hr</p>
 				</div>
 				<div class="membership-level-full cf">
-	              <p class="membership-level-left">1 for $40</p>
-	              <p class="membership-level-right no-border">This is our base price for cage rentals.</p>
+	              <p class="membership-level-left">Double A</p>
+	              <p class="membership-level-right no-border">Rent time as you need it. The cost is $40/hr rental of 1 tunnel.</p>
 				</div>
 				
               <span class="h-bar">&nbsp;</span>
 
               <h2>Hours of Operation:</h2>
               <div class="membership-level-full cf">
-	              <p class="membership-level-left">7 Days a Week:</p>
-	              <p class="membership-level-right">6:00am - 11:00pm</p>
+	              <p class="membership-level-left">Peak Hours:</p>
+	              <p class="membership-level-right">4:00pm-9:00pm Monday - Friday<br>
+              10:00am - 3:00pm Saturday &amp; Sunday</p>
               </div>
-              
+              <div class="membership-level-full cf">
+	              <p class="membership-level-left">Non-Peak Hours:</p>
+	              <p class="membership-level-right no-border">6:00am-4:00pm Monday - Friday<br>
+              6:00am-10:00am &amp; 3:00pm-11:00pm Saturday &amp; Sunday</p>
+			  </div>
               <a class="join-now-btn" href="https://booknow.appointment-plus.com/7qp3lmnc" target="_blank">Book Now!</a>
               <!-- these links are creating a horizontal scroll bar. -->
 				
@@ -87,15 +115,14 @@
           <div class="container">
              <a href="#" class="click no-default">2. Obtain your personal building access code <i class="fa fa-plus"></i></a>
             <div class="expand">
-              <p class="full-text ex-sp">Within 24 hours of booking your first batting cage rental you will receive your personal building access code that you can use for all your future rentals. Next time you want to book time just visit our site and schedule at your convenience.</p>
-              <!-- <p class="full-text note">Note: Only your 1st reservation requires this extra step. In the future you will always use the same access code.</p> -->
+              <p class="full-text">Once you have joined you will receive a building access code.</p>
             </div><!-- .expand !-->
           </div><!-- #container -->
 
           <div class="container">
              <a href="#" class="click no-default">3. Book reservations online anytime <i class="fa fa-plus"></i></a>
             <div class="expand">
-              <p class="full-text ex-sp">Our automated booking process lets you reserve a time and pay online whenever the mood <span class="strikethrough">strikes</span> (wrong choice of words) - whenever the mood hits!</p>
+              <p class="full-text">Our automated booking process lets you reserve a time and pay online whenever the mood <span class="strikethrough">strikes</span> (wrong choice of words) - whenever the mood hits!</p>
 
               <a class="join-now-btn" href="https://booknow.appointment-plus.com/7qp3lmnc" target="_blank">Book Now!</a>
 
@@ -105,7 +132,7 @@
 	</div><!-- #easy-as -->
 
 	<div class="add-resource cf">
-		<h2>Rookies - Where High Altitude Ball Players Come to Train</h2>
+		<h2>Rookies - Where Hight Altitude Ball Players Come to Train</h2>
 		<p class="full-width">Rookies LLC was established to provide an indoor pitching and hitting facility to the young baseball and softball players of Evergreen and the surrounding area.  Registered members are allowed to reserve time in the tunnels between 6:00am and 11:00pm 7 days a week, including holidays.  The reservations will be made in 1 hour blocks.  We require all members to straighten up their rented tunnel and vacate within the 60 minute time limit.  The training tunnels are equipped with pitching mounds/mats, batting tees, L-screens and pitching machines.  Tunnel 1 measures 14’W x 64’L and Tunnel 2 measures 12’W by 61’.</p>
 
 		<span class="h-bar">&nbsp;</span>
---
+++ js/google-map.js
@@ -0,0 +1,14 @@
+     function initialize() {
+    var map_canvas = document.getElementById('map');
+    var map_options = {
+        center: new google.maps.LatLng(39.636833, -105.316211),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(map_canvas, map_options);
+    var i=new google.maps.Marker({
+        position:new google.maps.LatLng(39.636833, -105.316211),
+        map:map
+        })
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
