e5b5c8432c09c412a91df96c4b123c978f29e463
robertmeans <robert@robertmeans.com>
2015-10-08 15:15:15 -0600

    downloaded beta directory
    because I couldn't find the freakin' files that were the correct ones to
    use - ?!

---
+++ _from beta directory/index.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="cache-control" content="no-cache">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="expires" content="0">
+	
+	<title>Rookies - Batting Cages in Evergreen, Colorado</title>
+	<link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
+	<link href='https://fonts.googleapis.com/css?family=Courgette|Lato' rel='stylesheet' type='text/css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js"></script>
+	<script src="js/google-map.js"></script>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div id="top-of-page"></div>
+<a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+
+<nav>
+	<ul>
+		<li><a href="#instructions" class="main-nav no-active-nav">Get Started</a></li>
+		<li><a href="#contact" class="main-nav">Contact</a></li>
+	</ul>
+</nav>
+
+<section id="intro">
+	<img src="_images/rookies-logo-02.png">
+	<h2>Evergreen, Colorad<a class="temp-link" href="http://www.rookiesevergreen.com/beta/Rookies-Waiver-and-Rules.pdf" target="_bank">o</a><br>Batting Cages</h2>
+	<div id="join-book">
+		<ul>
+			<!-- <li><a class="reservations" href="https://booknow.appointment-plus.com/7qp3lmnc" target="_blank">book now</a></li> -->
+			<li><a class="reservations no-default" href="#"  onclick='overlay()'>book now</a></li>
+			
+		</ul>
+	</div>
+</section><!-- #intro -->
+
+<article id="instructions">
+
+<div class="instruction-wrap cf">
+
+	<p class="easy-as">Getting started is as easy as...</p>
+
+	<div id="easy-as" class="cf">
+
+          <div class="container">
+            
+            <a href="#" class="click no-default">1. Book time in one of our batting cages <i class="fa fa-plus"></i></a>
+
+			<div class="expand">
+
+				<p class="full-text ex-sp">Book time by the hour or purchase one of our discounted package options and save.</p>
+
+              <h2>Booking Rates:</h2>
+				<div class="membership-level-full cf">
+	              <p class="membership-level-left">10 for $350</p>
+	              <p class="membership-level-right">Buy 10 1 hour sessions and save $50.</p>
+	              <p class="membership-level-right">When you buy a block of 10 sessions your account receives 10 credits that you can use towards future cage rentals at your convenience.</p>
+				</div>
+				<div class="membership-level-full cf">
+	              <p class="membership-level-left">4 for $150</p>
+	              <p class="membership-level-right">Buy 4 1 hour sessions and save $10.</p>
+	              <p class="membership-level-right">When you buy a block of 4 sessions your account receives 4 credits that you can use towards future cage rentals at your convenience.</p>
+				</div>
+				<div class="membership-level-full cf">
+	              <p class="membership-level-left">1 for $40</p>
+	              <p class="membership-level-right no-border">This is our base price for cage rentals.</p>
+				</div>
+				
+              <span class="h-bar">&nbsp;</span>
+
+              <h2>Hours of Operation:</h2>
+              <div class="membership-level-full cf">
+	              <p class="membership-level-left">7 Days a Week:</p>
+	              <p class="membership-level-right">6:00am - 11:00pm</p>
+              </div>
+              
+              <a class="join-now-btn no-default" href="#"  onclick='overlay()'>Book Now!</a>
+              <!-- these links are creating a horizontal scroll bar. -->
+				
+            </div><!-- .expand !-->
+          </div><!-- #container -->
+
+          <div class="container">
+             <a href="#" class="click no-default">2. Obtain your personal building access code <i class="fa fa-plus"></i></a>
+            <div class="expand">
+              <p class="full-text ex-sp">Within 24 hours of booking your first batting cage rental you will receive your personal building access code that you can use for all your future rentals. Next time you want to book time just visit our site and schedule at your convenience.</p>
+              <!-- <p class="full-text note">Note: Only your 1st reservation requires this extra step. In the future you will always use the same access code.</p> -->
+            </div><!-- .expand !-->
+          </div><!-- #container -->
+
+          <div class="container">
+             <a href="#" class="click no-default">3. Book reservations online anytime <i class="fa fa-plus"></i></a>
+            <div class="expand">
+              <p class="full-text ex-sp">Our automated booking process lets you reserve a time and pay online whenever the mood <span class="strikethrough">strikes</span> (wrong choice of words) - whenever the mood hits!</p>
+
+              <a class="join-now-btn no-default" href="#"  onclick='overlay()'>Book Now!</a>
+
+            </div><!-- .expand !-->
+          </div><!-- #container -->
+
+	</div><!-- #easy-as -->
+
+	<div class="add-resource cf">
+		<h2>Rookies - Where High Altitude Ball Players Come to Train</h2>
+		<p class="full-width">Rookies LLC was established to provide an indoor pitching and hitting facility to the young baseball and softball players of Evergreen and the surrounding area.  Registered members are allowed to reserve time in the tunnels between 6:00am and 11:00pm 7 days a week, including holidays.  The reservations will be made in 1 hour blocks.  We require all members to straighten up their rented tunnel and vacate within the 60 minute time limit.  The training tunnels are equipped with pitching mounds/mats, batting tees, L-screens and pitching machines.  Tunnel 1 measures 14’W x 64’L and Tunnel 2 measures 12’W by 61’.</p>
+
+		<span class="h-bar">&nbsp;</span>
+
+		<h2>Additional Resources</h2>
+
+		<p class="half">
+			Tom's Sandlot - Tom Peterson<br>
+			12150 W. 44<sup>th</sup> Avenue #102<br>
+			Wheat Ridge, CO 80033<br>
+			(303) 883-0474
+		</p>
+		<p class="half">
+			Catalyst Sport LLC<br>
+			15864 W. 6<sup>th</sup> Avenue Service Road<br>
+			Golden, CO 80401<br>
+			(303) 284-2191
+		</p>
+
+	</div><!-- .add-resource -->
+
+</div><!-- .instruction-wrap -->
+
+</article><!-- #instructions -->
+
+<footer id="contact" class="cf">
+	<div class="overlay" onClick="style.pointerEvents='none'"><!-- prevent wheel zoom over map --></div>
+	<div id="map"></div>
+
+	<div class="location">
+		<p><span class="sm-gone"><i class="fa fa-map-marker"></i></span><a class="loc" tabIndex="-1" href="https://www.google.com/maps/place/27888+Meadow+Dr,+Evergreen,+CO+80439/@39.6366669,-105.3163154,17z/data=!3m1!4b1!4m2!3m1!1s0x876b9e7502759af7:0x7a516115f5565751" target="_blank">27888 Meadow Drive<span class="sm-gone">, </span><br class="lg-gone">Evergreen, CO 80439</a></p>
+		<p><span class="sm-gone"><i class="fa fa-phone-square"></i></span><a class="loc" tabIndex="-1" href="tel:(626)%20253-0449">(626) 253-0449</a></p>
+	</div>
+	<div class="contact-form">
+		
+    <form action="formmail.php" method="post" id="contactForm" onSubmit="return validateEmail(document.forms[0].email.value);">
+        
+	    <ul>
+	        <li>
+	          <label class="text" for="name">Name</label>
+	          <input name="name" type="text" id="name" tabindex="10" />
+	        </li>
+	        <li>
+	          <label class="text" for="email">Email</label>
+	          <input name="email" type="email" id="email" tabindex="20" />
+	        </li>
+	        <li>
+	          <label class="text" for="comments">Comments</label>
+	          <textarea name="comments" id="comments" tabindex="30"></textarea>
+	        </li>
+	        <li>
+	            <input id="send" type="submit" value="Send" tabindex="40" />
+	        </li>
+	        
+	    </ul> 
+    
+    </form>
+
+	</div>
+	<div class="copyright cf">
+		<p>&copy;2015 Rookies LLC | <a href="http://www.evergreenwebdesign.com" target="_blank">Evergreen Web Design</a></p>
+	</div>
+</footer>
+
+<section id="h-space"></section>
+
+<div id="overlay">
+	
+     <div class="body-txt">
+
+     <!-- <a class="modal-close no-default" href='#' onclick='overlay()'>X</a> -->
+     <p><a class="modal-print" href="Rookies-Waiver-and-Rules.pdf" target="_blank">Print</a></p>
+          <h2>Waiver of Liability and Release for the use of Rookies LLC</h2>
+          <p>I, the undersigned participant /parent or legal guardian of the participant
+			recognize and acknowledge that the activities at Rookies, involve risk of serious
+			injury, including permanent disability or death, and serve social and economic
+			losses which might result from participant action, inaction, negligence of others,
+			rules of play, or the condition of the premises or any equipment used there on.  
+			Further, I understand that there may be other risks not known or reasonably
+			foreseeable at this time and that such risks shall be assumed by the undersigned.  
+			In my absence, I authorize the instructor, coach, or owner of Rookies to call for
+			emergency should they be necessary in the case of injury or suspected injury, or
+			during the times that the above named individual is participating in activity at
+			Rookies.  I will accept responsibility for the payment of any and all treatment
+			provided therein including emergency rescue services.</p>
+
+			<p>I certify that the above name is capable of participating safely in the activities at
+			Rookies.  I understand that the owners of Rookies do not provide accident, health,
+			or life insurance coverage for the above named participant during program
+			participation.  I further understand that I am legally responsible for actions of the
+			above named individual including, but not limited to, any damage to private or
+			public property.  I am legally responsible for my own and/or my child’s welfare
+			and actions including personal needs and medical expenses.  I agree to indemnify
+			and hold the owners of Rookies and its representatives harmless from any loss,
+			damage or injury which may result from me or my child’s participation in activities
+			at Rookies.  The release of liability and indemnity applies equally to losses,
+			damages or injuries caused or alleged to be caused in whole or in part by the
+			negligence of the owners of Rookies.  I further agree to release, waive and
+			discharge, and covenant not to sue the owners of Rookies for any claims,
+			demands, or actions whatsoever arising out of any damage, loss or injury incurred
+			on or to me or my child as a result of my participation or my child’s in activities at
+			Rookies.  This release of liability and indemnity applies to me, the undersigned, or
+			my child, as well as any personal representatives, assigns, heirs and next of kin.</p>
+
+
+			<h2>Rookies Rules and Regulations</h2>
+			<ul>
+				<li>If you are registered and have been provided an access code to
+					Rookies, you are individually responsible for any and all persons
+					you allow access into the facility regarding injury, theft or
+					destruction of Rookies property.</li>
+				<li>Youth under age 16 must be accompanied by an adult 18 years of
+					age or older at all times.</li>
+				<li>Only persons age 16 or older are permitted to feed balls into the
+					pitching machines.</li>
+				<li>Batters must always wear a batting helmet when a pitching
+					machine is being used.</li>
+				<li>You must stay behind the safety screen when operating a pitching
+					machine or throwing live batting practice.</li>
+				<li>Individuals not pitching, catching or hitting must remain outside
+					of the netted tunnel.</li>
+				<li>Closed – toe shoes must be worn at all times.</li>
+				<li>No molded baseball/softball cleats or spikes are permitted at any
+					time.</li>
+			</ul>
+ 
+     </div><!-- .body-txt -->
+     <div id="mheader"><p><a class="modal-close-btm no-default" href='#' onclick='overlay()'>Decline</a> <a class="modal-close-btm" href="https://booknow.appointment-plus.com/7qp3lmnc" target="_blank" onclick='overlay()'>I Agree</a></p></div>
+</div>
+
+
+<script src="js/scripts.js?<?php echo time(); ?>"></script>
+<script src="http://localhost:35729/livereload.js"></script>
+</body>
+</html>
