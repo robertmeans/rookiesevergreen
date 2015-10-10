6fe4ecc4b1f98b5e4a64c6dd95103d155dea0816
robertmeans <robert@robertmeans.com>
2015-10-10 09:34:58 -0600

    added favicon & Google Analytics

--- index.php
+++ index.php
@@ -7,16 +7,21 @@
 	<meta http-equiv="expires" content="0">
 	
 	<title>Rookies - Batting Cages in Evergreen, Colorado</title>
-	<link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css">
+	<link rel="icon" type="image/ico" href="_images/favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
 	<link href='https://fonts.googleapis.com/css?family=Courgette|Lato' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="style.css?<?php echo time(); ?>" type="text/css">
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js"></script>
 	<script src="js/google-map.js"></script>
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 </head>
 <body>
+<?php include_once("_includes/analyticstracking.php") ?>
+
 <div id="top-of-page"></div>
 <a href="#" class="back-to-top"><i class="fa fa-angle-up"></i></a>
 
